c2fd8da1ec21617f9744b468b52ea6af3b5d11ae
Сельченков Николай <undwad@integra-s.com>
2013-10-11 12:06:16 +0400

    +

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
-//#include "../inc/sysdefs.h"
-
-#include <iostream>
-#include <string>
 #include <memory>
 
 #include "lua.hpp"
-
-//extern "C" {
 #include "conf0.h"
-//}
-
-using namespace std;
 
 void main()
 {
-	shared_ptr<lua_State> L(luaL_newstate(), lua_close);
+	std::shared_ptr<lua_State> L(luaL_newstate(), lua_close);
 	luaL_openlibs(&*L);
 	luaopen_conf0(&*L);
     if(luaL_loadfile(&*L, "test.lua") || lua_pcall(&*L, 0, 0, 0))    
