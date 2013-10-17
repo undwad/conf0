f5b6f6e67a5e11687f5141ec4f4532e1d8169bd4
Сельченков Николай <undwad@integra-s.com>
2013-10-17 16:53:09 +0400

    +

--- output/test.lua
+++ output/test.lua
@@ -45,36 +45,38 @@ function execute(params)
 	end
 end
 
+function passref(ref) return 'avahi' == conf0.backend and ref or nil end
+
 execute{proc = conf0.register_, type = "_http._tcp", name = 'conf0test', port = port2opaque(5500), callback = function(res) print(prettytostring(res)) end}
 
 --local type = '_rtsp._tcp'
 local type = '_http._tcp'
 
 local items = {}
-execute{proc = conf0.browse, type = type, callback = function(i) 
-	if i.name and i.type and i.domain and not items[i.name] then
-		items[i.name] = i
-		execute{proc = conf0.resolve, ref = 'avahi' == conf0.backend and i.ref or nil, interface_ = i.interface_, protocol = i.protocol, name = i.name, type = i.type, domain = i.domain, callback = function(j)
-			for k,v in pairs(j) do i[k] = v end
-			if i.opaqueport then
-				i.port = opaque2port(i.opaqueport)
-			end
-			if 'bonjour' == conf0.backend then
-				if j.host then
-					execute{proc = conf0.query, ref = 'avahi' == conf0.backend and j.ref or nil, fullname = j.host, type = conf0.types.A, class_ = conf0.classes.IN, callback = function(k)
-						i.ip = inet_ntoa(k.data)
-						print(prettytostring(i))
-						return true
-					end}
-				end
-			else
-				print(prettytostring(i))
-			end
-			return true
-		end}
-	end
-	return 'avahi' == conf0.backend and conf0.events.BROWSER_ALL_FOR_NOW == i.event_
-end}
+--execute{proc = conf0.browse, type = type, callback = function(i) 
+--	if i.name and i.type and i.domain and not items[i.name] then
+--		items[i.name] = i
+--		execute{proc = conf0.resolve, ref = passref(i.ref), interface_ = i.interface_, protocol = i.protocol, name = i.name, type = i.type, domain = i.domain, callback = function(j)
+--			for k,v in pairs(j) do i[k] = v end
+--			if i.opaqueport then
+--				i.port = opaque2port(i.opaqueport)
+--			end
+--			if 'bonjour' == conf0.backend then
+--				if j.host then
+--					execute{proc = conf0.query, ref = passref(j.ref), fullname = j.host, type = conf0.types.A, class_ = conf0.classes.IN, callback = function(k)
+--						i.ip = inet_ntoa(k.data)
+--						print(prettytostring(i))
+--						return true
+--					end}
+--				end
+--			else
+--				print(prettytostring(i))
+--			end
+--			return true
+--		end}
+--	end
+--	return 'avahi' == conf0.backend and conf0.events.BROWSER_ALL_FOR_NOW == i.event_
+--end}
 
 	
 
