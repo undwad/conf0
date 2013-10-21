c8fb7ca7a569b20d9d4d6b86f38b9b84e1c92746
Сельченков Николай <undwad@integra-s.com>
2013-10-21 15:47:45 +0400

    +

--- output/browse0conf.lua
+++ output/browse0conf.lua
@@ -1,55 +1,16 @@
-require 'conf0'
+require 'conf0' -- load conf0 module
  
-local function testflag(set, flag) return set % (2 * flag) >= flag end
+-- tests if flag is set
+function testflag(set, flag) return set % (2 * flag) >= flag end
 
-local function text2list(text, textlen)
-	if not textlen then textlen = string.len(text) end
-	local list = {}
-	local i = 1
-	while i <= textlen do
-		local len = string.byte(text[i])
-		if len > 0 then list[#list + 1] = string.sub(text, i + 1, i + len) end
-		i = i + len + 1
-	end
-	return list
-end
+-- we the following only on bonjour since avahi does not use opaque port and resolves service ip
+if 'bonjour' == conf0.backend then
+	
+	-- converts address as integer to string in the form of ip
+	function inet_ntoa(addr) return string.byte(addr[1])..'.'..string.byte(addr[2])..'.'..string.byte(addr[3])..'.'..string.byte(addr[4]) end
 
-if 'avahi' == conf0.backend then
-	return function(params)
-		local connection = conf0.connect{callback = function(connected)
-			local callback = params.callback
-			params.ref = connected.ref
-			params.callback = function(browsed)
-				if conf0.events.BROWSER_CACHE_EXHAUSTED == browsed.event_ then
-				elseif conf0.events.BROWSER_ALL_FOR_NOW == browsed.event_ then
-					conf0.disconnect{ref = connected.ref}
-				else
-					browsed.new = conf0.events.BROWSER_NEW == browsed.event_
-					browsed.remove = conf0.events.BROWSER_REMOVE == browsed.event_
-					browsed.error = conf0.events.BROWSER_FAILURE == browsed.event_
-					browsed.callback = nil
-					if callback(browsed) then
-						local resolver
-						browsed.flags = nil
-						browsed.ref = connected.ref
-						browsed.callback = function(resolved)
-							for k,v in pairs(resolved) do browsed[k] = v end
-							browsed.list = text2list(browsed.text)
-							browsed.error = conf0.events.RESOLVER_FAILURE == browsed.event_
-							callback(browsed)
-						end
-						local resolver = conf0.resolve(browsed)
-					end
-				end
-			end
-			local browser = conf0.browse(params)
-		end}
-		while connection and conf0.iterate{ref = connection} do end
-	end
-elseif 'bonjour' == conf0.backend then
-	local byte = string.byte
-	local function inet_ntoa(addr) return byte(addr[1])..'.'..byte(addr[2])..'.'..byte(addr[3])..'.'..byte(addr[4]) end
-	local function tobytes(value, num)
+	-- converts integer to byte array
+	function tobytes(value, num)
 		local bytes = ""
 		num = num or 4
 		for i = 1, num do
@@ -58,41 +19,145 @@ elseif 'bonjour' == conf0.backend then
 		end
 		return bytes
 	end
-	local function opaque2port(port)
+
+	-- converts opaque value to port
+	function opaque2port(port)
 		local bytes = tobytes(port)
-		return byte(bytes, 4) * 256 + byte(bytes, 3)
+		return string.byte(bytes, 4) * 256 + string.byte(bytes, 3)
 	end
-	return function(params)
-		if 'table' ~= type(params) then error('function only accepts single parameter of type table') end
-		if 'function' ~= type(params.callback) then error('callback named parameter missing') end
-		local callback = params.callback
-		params.callback = function(browsed)
-			browsed.new = testflag(browsed.flags, conf0.flags.Add)
-			browsed.remove = not browsed.new
-			browsed.callback = nil
-			if callback(browsed) then
-				local resolver
-				browsed.flags = nil
-				browsed.callback = function(resolved)
-					for k,v in pairs(resolved) do browsed[k] = v end
-					browsed.list = text2list(browsed.text)
-					browsed.port = opaque2port(browsed.opaqueport)		
-					local query
-					query = conf0.query{fullname = browsed.host, type = conf0.types.A, class_ = conf0.classes.IN, callback = function(result)
-						browsed.ip = inet_ntoa(result.data)
-						browsed.callback = nil
-						callback(browsed)
-						query = nil
-					end}
-					while query and conf0.iterate{ref = query} do end
-					resolver = nil
-				end
-				resolver = conf0.resolve(browsed)
-				while resolver and conf0.iterate{ref = resolver} do end
+
+	-- converts port to opaque value
+	port2opaque = opaque2port
+
+elseif 'avahi' == conf0.backend then
+
+else -- unknown backend value
+	error('invalid conf0 backend '..conf0.backend)
+end
+
+-- executes service function and wait for callback
+function conf0.execute(params)
+	local ref -- variable for service reference
+	local callback = params.callback -- saves callback passed by user
+	params.callback = function(res) -- new local callback proc
+		if callback(res) then -- if user's callback returns true 
+			ref = nil -- then we don't need service reference anymore
+		end
+	end
+	ref = params.proc(params) -- executes service function
+	if not params.ref then -- if succeeded and while 
+		while ref and conf0.iterate{ref=ref} do end
+	end
+	return ref -- returns service reference
+end
+
+return function(params)
+	-- parameters check
+	if 'table' ~= type(params) then error('function only accepts single parameter of type table') end
+	if 'function' ~= type(params.callback) then error('callback named parameter missing') end
+	local callback = params.callback -- saves user callback since we will use params as conf0.browse argument
+	params.callback = function(browsed)
+	end
+	conf0.execute{ -- begin calling conf0.connect 
+		proc = conf0.connect, -- service function to call
+		callback = function(connection) -- required conf0.connect callback
+			-- checks connection state
+			if connection and ('bonjour' == conf0.backend or ('avahi' == conf0.backend and conf0.states.CLIENT_S_RUNNING == connection.state)) then
+				print(connection)
 			end
 		end
-		local browser = conf0.browse(params)
-		while browser and conf0.iterate{ref = browser} do end
-	end 
-else error('invalid conf0 backend '..conf0.backend) end
+	} -- end calling conf0.connect 
+end
 
+--[[
+execute{ -- begin calling conf0.connect 
+	proc = conf0.connect, -- service function to call
+	callback = function(res) -- required conf0.connect callback
+		local items = {} -- table for browsable services
+		execute{ -- begin calling conf0.browse 
+			proc = conf0.browse, -- service function to call
+			ref = res.ref, -- service reference
+			type = type, -- service type
+			callback = function(i) -- begin browse callback
+				-- if browse result is valid service and yet isn't reported 
+				if i.name and i.type and i.domain and not items[i.name] then 
+					items[i.name] = i -- then store its info in the table
+					execute{ -- begin calling conf0.resolve 
+						proc = conf0.resolve, -- service function to call
+						ref = res.ref, -- service reference
+						interface_ = i.interface_, -- service interface
+						protocol = i.protocol, -- service protocol
+						name = i.name, -- service name
+						type = i.type, -- service type
+						domain = i.domain, -- service domain
+						callback = function(j) -- begin resolve callback
+							for k,v in pairs(j) do i[k] = v end -- copy all fields of resolve result into query result
+							if i.opaqueport then -- if has opaque port 
+								i.port = opaque2port(i.opaqueport) -- then we need to decode it
+							end
+							-- since avahi resolves ip address
+							-- we need to query service record for ip address only on bonjour
+							if 'bonjour' == conf0.backend then -- on bonjour
+								if j.host then
+									execute{ -- begin calling conf0.query 
+										proc = conf0.query, -- service function to call
+										ref = res.ref, -- service reference
+										fullname = j.host, -- record name is host name for this request
+										type = conf0.types.A, -- record type for address (see conf0.types)
+										class_ = conf0.classes.IN, -- record class (see conf0.classes)
+										callback = function(k) -- begin query callback
+											i.ip = inet_ntoa(k.data) -- converts ip address to string
+											print(prettytostring(i)) -- we can print service info right now
+											return true -- returns true to tell execute method that the appropriate callback was successfuly called
+										end -- end query callback
+									} -- end calling conf0.query 
+								end
+							else -- on avahi
+								print(prettytostring(i)) -- we can print service info right now
+							end
+							return true -- returns true to tell execute method that the appropriate callback was successfuly called
+						end -- begin resolve callback
+					} -- end calling conf0.resolve 
+				end
+				-- on avahi we need to stop iteration manually
+				if 'avahi' == conf0.backend and conf0.events.BROWSER_ALL_FOR_NOW == i.event_ then
+					conf0.disconnect{ -- calling conf0.disconnect 
+						ref=res.ref -- service reference
+					}
+				end
+			end -- end browse callback
+		} -- end calling conf0.browse 
+	end
+} -- end calling conf0.connect 
+
+		--browsed.new = testflag(browsed.flags, conf0.flags.Add)
+		--browsed.remove = not browsed.new
+
+				--if conf0.events.BROWSER_CACHE_EXHAUSTED == browsed.event_ then
+				--elseif conf0.events.BROWSER_ALL_FOR_NOW == browsed.event_ then
+--					conf0.disconnect{ref = connected.ref}
+	--			else
+		--			browsed.new = conf0.events.BROWSER_NEW == browsed.event_
+			--		browsed.remove = conf0.events.BROWSER_REMOVE == browsed.event_
+			--		browsed.error = conf0.events.BROWSER_FAILURE == browsed.event_
+
+							--browsed.error = conf0.events.RESOLVER_FAILURE == browsed.event_
+
+
+
+--		execute{ -- begin calling conf0.register_ 
+--			proc = conf0.register_, -- service function to call
+--			ref = res.ref, -- service reference
+--			type = type, -- service type
+--			name = name, -- service name
+--			port = port2opaque(port), -- service port
+--			callback = function(res) -- begin register callback
+--				print('registering', prettytostring(res)) -- print result
+--				-- check res.flags with conf0.flags on bonjour
+--				-- check res.state with conf0.state on avahi
+--			end -- end register callback
+--		} -- end calling conf0.register_ 
+--]]
+
+-- many functions have flags, interface, protocol etc. parameters that are set to default values
+-- check this in bonjour / avahi documentations and in conf0.flags, conf0.interfaces, conf0.protocols etc.
--- output/test.lua
+++ output/test.lua
@@ -1,134 +1,15 @@
 require 'std' -- load lua standard library for pretty printing etc.
-require 'conf0' -- load conf0 module
+browse = require 'browse0conf' -- load browse0conf module
 
 print(prettytostring(conf0)) -- print conf0 module to see what we have
 
--- we the following only on bonjour since avahi does not use opaque port and resolves service ip
-if 'bonjour' == conf0.backend then 
-	
-	byte = string.byte
-
-	-- converts address as integer to string in the form of ip
-	function inet_ntoa(addr) return byte(addr[1])..'.'..byte(addr[2])..'.'..byte(addr[3])..'.'..byte(addr[4]) end
-
-	-- converts integer to byte array
-	function tobytes(value, num)
-		local bytes = ""
-		num = num or 4
-		for i = 1, num do
-			bytes = string.char(value % 256) .. bytes
-			value = math.floor(value / 256)
-		end
-		return bytes
-	end
-
-	-- converts port to opaque port
-	function opaque2port(port)
-		local bytes = tobytes(port)
-		return byte(bytes, 4) * 256 + byte(bytes, 3)
-	end
-
-else 
-	-- on avahi we don't need port conversion
-	function opaque2port(port) return port end
-end
-
-port2opaque = opaque2port
-
--- executes service function and wait for callback
-function execute(params)
-	local ref -- variable for service reference
-	local callback = params.callback -- saves callback passed by user
-	params.callback = function(res) -- new local callback proc
-		if callback(res) then -- if user's callback returns true 
-			ref = nil -- then we don't need service reference anymore
-		end
-	end
-	ref = params.proc(params) -- executes service function
-	if not params.ref then -- if succeeded and while 
-		while ref and conf0.iterate{ref=ref} do end
-	end
-	return ref -- returns service reference
-end
 
 local type = '_rtsp._tcp'
 --local type = '_http._tcp'
 local port = 5500 -- port of service to register
 local name = 'conf0test' -- name of service to register
 
-execute{ -- begin calling conf0.connect 
-	proc = conf0.connect, -- service function to call
-	--flags = 0, -- optional flags (avahi only) see conf0.flags
-	callback = function(res) -- required conf0.connect callback
-		execute{ -- begin calling conf0.register_ 
-			proc = conf0.register_, -- service function to call
-			ref = res.ref, -- service reference
-			type = type, -- service type
-			name = name, -- service name
-			port = port2opaque(port), -- service port
-			callback = function(res) -- begin register callback
-				print('registering', prettytostring(res)) -- print result
-				-- check res.flags with conf0.flags on bonjour
-				-- check res.state with conf0.state on avahi
-			end -- end register callback
-		} -- end calling conf0.register_ 
-		local items = {} -- table for browsable services
-		execute{ -- begin calling conf0.browse 
-			proc = conf0.browse, -- service function to call
-			ref = res.ref, -- service reference
-			type = type, -- service type
-			callback = function(i) -- begin browse callback
-				-- if browse result is valid service and yet isn't reported 
-				if i.name and i.type and i.domain and not items[i.name] then 
-					items[i.name] = i -- then store its info in the table
-					execute{ -- begin calling conf0.resolve 
-						proc = conf0.resolve, -- service function to call
-						ref = res.ref, -- service reference
-						interface_ = i.interface_, -- service interface
-						protocol = i.protocol, -- service protocol
-						name = i.name, -- service name
-						type = i.type, -- service type
-						domain = i.domain, -- service domain
-						callback = function(j) -- begin resolve callback
-							for k,v in pairs(j) do i[k] = v end -- copy all fields of resolve result into query result
-							if i.opaqueport then -- if has opaque port 
-								i.port = opaque2port(i.opaqueport) -- then we need to decode it
-							end
-							-- since avahi resolves ip address
-							-- we need to query service record for ip address only on bonjour
-							if 'bonjour' == conf0.backend then -- on bonjour
-								if j.host then
-									execute{ -- begin calling conf0.query 
-										proc = conf0.query, -- service function to call
-										ref = res.ref, -- service reference
-										fullname = j.host, -- record name is host name for this request
-										type = conf0.types.A, -- record type for address (see conf0.types)
-										class_ = conf0.classes.IN, -- record class (see conf0.classes)
-										callback = function(k) -- begin query callback
-											i.ip = inet_ntoa(k.data) -- converts ip address to string
-											print(prettytostring(i)) -- we can print service info right now
-											return true -- returns true to tell execute method that the appropriate callback was successfuly called
-										end -- end query callback
-									} -- end calling conf0.query 
-								end
-							else -- on avahi
-								print(prettytostring(i)) -- we can print service info right now
-							end
-							return true -- returns true to tell execute method that the appropriate callback was successfuly called
-						end -- begin resolve callback
-					} -- end calling conf0.resolve 
-				end
-				-- on avahi we need to stop iteration manually
-				if 'avahi' == conf0.backend and conf0.events.BROWSER_ALL_FOR_NOW == i.event_ then
-					conf0.disconnect{ -- calling conf0.disconnect 
-						ref=res.ref -- service reference
-					}
-				end
-			end -- end browse callback
-		} -- end calling conf0.browse 
-	end
-} -- end calling conf0.connect 
+browse{type = type, callback = function(svc)
 
--- many functions have flags, interface, protocol etc. parameters that are set to default values
--- check this in bonjour / avahi documentations and in conf0.flags, conf0.interfaces, conf0.protocols etc.
+end}
 
